caa2f6b8402b101fc4cd966c776993c4108cce63
Alexander Embiricos <alexanderembiricos@gmail.com>
2014-03-09 15:26:09 -0700

    landing modal

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,35 @@ var models = require('../models');
  * GET the main index page
  */
 
-exports.view = function(req, res){
-	models.Story
-
-    .find({})
-    .sort('updated_date')
-    .exec(renderIndex);
-
-  function renderIndex(err, stories) {
-  	if (err) console.log(err);
-
-  	for (var i=0; i<stories.length; i++) {
-  		stories[i].add_clearfix_after = (i+1) % 3 == 0;
-  	}
-
-	  // Render the page
-	  res.render('index', {
-	  	title: 'Stand Together',
-	  	stories: stories
-	  });
-  }
+exports.index = function(req, res){
+  renderIndex(req, res, false);
 };
+
+
+/*
+ * GET the landing page, which is the index plus an extra modal
+ */
+
+exports.landing = function(req, res) {
+  renderIndex(req, res, true);
+};
+
+function renderIndex(req, res, hasLanding) {
+  models.Story
+  .find({})
+  .sort('updated_date')
+  .exec(function(err, stories) {
+    if (err) console.log(err);
+
+    for (var i=0; i<stories.length; i++) {
+      stories[i].add_clearfix_after = (i+1) % 3 == 0;
+    }
+
+    // Render the page
+    res.render('index', {
+      title: 'Stand Together',
+      stories: stories,
+      hasLanding: hasLanding
+    });
+  });
+}
